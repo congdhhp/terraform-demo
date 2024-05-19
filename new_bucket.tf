c971c519d60ae1ede23a51e77c389439381376e4
Cong Do <congdh5298@gmail.com>
2024-05-19 16:58:53 +0700

    update new_bucket.tf
    Signed-off-by: Cong Do <congdh5298@gmail.com>

--- new_bucket.tf
+++ new_bucket.tf
@@ -23,3 +23,25 @@ resource "aws_instance" "ec2_instance" {
     Name = "CICD test instance"
   }
 }
+
+# Output for S3 bucket
+output "bucket_name" {
+  value = aws_s3_bucket.udabucket.bucket
+  description = "The name of the bucket"
+}
+
+# Output for EC2 instance ID
+output "instance_id" {
+  value = aws_instance.ec2_instance.id
+  description = "The ID of the EC2 instance"
+}
+
+# Placeholder outputs for Blue/Green deployment status
+# These need to be adapted based on your actual deployment strategy
+output "blue_status" {
+  value = "live"  # This should reflect the actual condition or state from your infrastructure
+}
+
+output "green_status" {
+  value = "inactive"  # This should reflect the actual condition or state from your infrastructure
+}
